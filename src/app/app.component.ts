5f3ca079893320af381cee941806f7e4e6172132
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-04-09 15:38:49 +0100

    component home finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; // Import the NavbarComponent
 import { FooterComponent } from './footer/footer.component'; // Import the FooterComponent
+import { HomeComponent } from './home/home.component'; // Import the HomeComponent
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
