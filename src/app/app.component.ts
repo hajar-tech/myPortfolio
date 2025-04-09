2df7a3899394327c1a4c3a1c125c2c02a5451224
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-04-09 11:36:23 +0100

    footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; // Import the NavbarComponent
+import { FooterComponent } from './footer/footer.component'; // Import the FooterComponent
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
