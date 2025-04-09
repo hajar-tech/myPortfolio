d5eb4f1450e43b00e3713658a746494fdacb9d23
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-04-09 16:59:38 +0100

    create contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; // Import the NavbarComponent
 import { FooterComponent } from './footer/footer.component'; // Import the FooterComponent
 import { HomeComponent } from './home/home.component'; // Import the HomeComponent
+import { ContactComponent } from './contact/contact.component'; // Import the ContactComponent
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,FooterComponent,HomeComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,HomeComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
